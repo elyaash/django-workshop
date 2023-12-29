a0620b7616e5841553f99c0023bb7a4352be0636
Elyaash <elyaash49@gmail.com>
2023-12-28 23:07:58 -0500

    refactored

--- sales/admin.py
+++ sales/admin.py
@@ -1,20 +1,84 @@
-from django.contrib import admin
-from .models import Product,Sales,Csv
+from django.contrib import admin, messages
+from django.db.models.aggregates import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from . import models
 
 # Register your models here.
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ["title","description","unit_price",]
-    fields = ["title","description","unit_price","image"]
-    search_fields = ["title"]
+@admin.register(models.Promotion)
+class PromotionAdmin(admin.ModelAdmin):
+    list_display= ["description","discount"]
+    search_fields = ['description']
 
-@admin.register(Sales)
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions = ['clear_inventory']
+    #nlines = [ProductImageInline]
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    #list_filter = ['collection', 'last_update', InventoryFilter]
+    list_per_page = 10
+    list_select_related = ['collection']
+    search_fields = ['title']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were successfully updated.',
+            messages.ERROR
+        )
+
+    # class Media:
+    #     css = {
+    #         'all': ['store/styles.css']
+    #     }
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['featured_product']
+    list_display = ['title', 'products_count']
+    search_fields = ['title']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (
+            reverse('admin:store_product_changelist')
+            + '?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            }))
+        return format_html('<a href="{}">{} Products</a>', url, collection.products_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('products')
+        )
+
+
+@admin.register(models.Sales)
 class SalesAdmin(admin.ModelAdmin):
     list_display = ['product','salesmen','sales_date','qty']
     fields = ['product','salesmen','sales_date','qty']
     list_select_related = ['product','salesmen']
 
-@admin.register(Csv)    
+@admin.register(models.Csv)    
 class CsvAdmin(admin.ModelAdmin):
     list_display = ['filename','status','upload_at']
     fields = ['filename','status','upload_at']
--- sales/models.py
+++ sales/models.py
@@ -1,12 +1,45 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from django.contrib.auth.models import User
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
 class Product(models.Model):
-    title = models.CharField(max_length = 100)
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    qty = models.IntegerField()
-    image = models.ImageField()
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(1)])
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(
+        Collection, on_delete=models.PROTECT, related_name='products')
+    promotions = models.ManyToManyField(Promotion, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
 
 class Csv(models.Model):
     STATUS_CHOICE_DEFAULT = 'P'
