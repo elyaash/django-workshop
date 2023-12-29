28f88ec8d8c3e651453d35f430d54253c2f3184e
Elyaash <elyaash49@gmail.com>
2023-12-29 00:24:05 -0500

    commit 4

--- sales/forms.py
+++ sales/forms.py
@@ -1,12 +1,22 @@
 from typing import Any
 from django.forms import ModelForm
-from .models import Csv
-
+from .models import Csv,Sales
+import csv
+import io
 
 class CsvModelFrom(ModelForm):
     def save(self, commit: bool = ...) -> Any:
         #print(self.data)
-        print(self.files["filename"])
+        csvFile = self.files.get('filename','')
+        if (len(csvFile) > 1):
+            file = csvFile.read().decode('utf-8-sig')  
+            reader = csv.DictReader(io.StringIO(file))
+            for row in reader:
+                Sales.objects.create(
+                    product_id=int(row.get("Product")),
+                    salesmen_id=int(row.get("User")),
+                    qty=int(row.get("Qty")),
+                )           
         return super().save(commit)
     class Meta:
         model = Csv
