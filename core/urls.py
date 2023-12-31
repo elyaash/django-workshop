714d612283c22b650e238ff88f8809ab7a4846ae
Elyaash <elyaash49@gmail.com>
2023-12-31 03:23:12 -0500

    login functionality

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,11 @@
 from django.views.generic import TemplateView
 from django.urls import path
-
+from .views import logout,login
 # URLConf
 urlpatterns = [
     path('', TemplateView.as_view(template_name='core/index.html'),name='home'),
     path('about-us', TemplateView.as_view(template_name='core/pages/about_us.html'),name='about-us'),
     path('contact-us', TemplateView.as_view(template_name='core/pages/contact_us.html'),name='contact-us'),
-    path('login', TemplateView.as_view(template_name='core/pages/login.html'),name='login'),
-
+    path('login', login,name='login'),
+    path('logout',logout, name='logout')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as login_user, logout as logout_user
+from django.contrib import messages
 
+# about us 
 
+def login(request):
+    if request.method == "GET" :
+        return render(request,'core/pages/login.html', {messages:messages})
+    print(request.method)
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    print(user)
+    if user is not None:
+        login_user(request,user)
+        return redirect('home')
+    else:
+        messages.error(request,"Username and password doesn't match")
+        return render(request,'core/pages/login.html')
+
+def logout(request):
+    logout_user(request)
+    return redirect('home')
 # custom 404 view
 def custom_404(request, exception):
     raise Exception("Sorry, no numbers below zero")
