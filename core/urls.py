b6406ba51d3b30e25596085c850fd3d3f6248675
Elyaash <elyaash49@gmail.com>
2023-12-30 20:53:46 -0500

    Implemented site layout

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 
 # URLConf
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='core/index.html'))
+    path('', TemplateView.as_view(template_name='core/index.html'),name='home'),
+    path('about-us', TemplateView.as_view(template_name='core/pages/about_us.html'),name='about-us'),
+    path('contact-us', TemplateView.as_view(template_name='core/pages/contact_us.html'),name='contact-us'),
+    path('login', TemplateView.as_view(template_name='core/pages/login.html'),name='login'),
+
 ]
